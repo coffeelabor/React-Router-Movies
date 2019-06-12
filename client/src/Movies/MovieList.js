ed0ec782dd55a45b7d7a97176922b47a4c66bf5e
Reed James <reedtjames1234@gmail.com>
2019-06-11 20:09:59 -0500

    working on save

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -26,34 +26,9 @@ export default class MovieList extends Component {
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
-          // <MovieDetails key={movie.id} movie={movie} />
           <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
     );
   }
 }
-
-// function MovieDetails({ movie }) {
-//   const { title, director, metascore, stars } = movie;
-//   return (
-//     <Link to={`/movies/${movie.id}`}>
-//       <div className="movie-card">
-//         <h2>{title}</h2>
-//         <div className="movie-director">
-//           Director: <em>{director}</em>
-//         </div>
-//         <div className="movie-metascore">
-//           Metascore: <strong>{metascore}</strong>
-//         </div>
-//         <h3>Actors</h3>
-
-//         {stars.map(star => (
-//           <div key={star} className="movie-star">
-//             {star}
-//           </div>
-//         ))}
-//       </div>
-//     </Link>
-//   );
-// }
